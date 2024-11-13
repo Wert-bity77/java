379e050a2a07a170aadc8ee9906c91f44ab2847e
Wert-bity77 <kabulovt707@gmail.com>
2024-11-13 17:37:18 +0500

    main va index
    wwww
    \

---
+++ main.js
@@ -0,0 +1,62 @@
+    // number = 0
+// var number = 20
+// console.log(number);
+// let numb = 100
+// numb = 10
+// console.log(numb);
+// const numbers = 15
+// console.log(numbers);
+
+
+// let savol = "wer"
+// let savol1 = 'ronaldo'
+
+// console.log(savol);
+
+let ismi = prompt('Ismingizni Nima?')
+let yosh = prompt(' Yoshingiz nechida?')
+let surname = prompt('Familiyangzni kriting?')
+let redbull = prompt ('Ronaldo or messi?')
+let fastfood = prompt ('Yoqtirgan faningiz') 
+let newYork = prompt ('emiraka aslida ozi kim?')
+let nechtaOdam = prompt ('Marsga nechta odam olib keldingiz?')
+let brend = prompt ('Sizga qaysi brend yoqadi')
+let n1 = prompt ('Eng yaxshi oyin qaysi?')
+let mobi = prompt ('Ohirgi ishlatgan tarmoqongiz?')
+let it  = prompt ('Nega aynan mars it?')
+let music = prompt ('Yoqtirgan musiqangiz?')
+let kino = prompt ('Ohirgi korgan kinoyingiz?')
+let ee = prompt ('Yaxshi korgan ovqatingiz')
+let fasl = prompt  ('Qaysi fasilni yoqtirasiz?')
+let tanlash = prompt ('coddy camp or Mars it')
+let team = prompt ('Real madrid or Barcelona')
+let bro  = prompt ('Eng yaqin dostingiz?')
+let hobbi  = prompt ('Sizning hobbiyingiz?')
+let language = prompt ('Qaysi tilni yaxshi bilasiz?')
+ 
+alert ('Sizni ismingiz  ' + ismi)
+alert('Yoshingiz  ' + yosh)
+alert('Sizni familiyangiz  ' + surname)
+alert('GOAT  '  +redbull)
+alert ('Faningiz  ' + fastfood)
+alert ( 'Shunaqa odam  ' + newYork)
+alert  ( 'Odam soni  ' + nechtaOdam)
+alert ('Brendingiz  '  + brend)
+alert ('eng yaxshi oyin  '  +  n1)
+alert ('mobile  '   + mobi)
+alert ('sababi  '  + it)
+alert ('musiqangiz  '   +  music)
+alert ('kinoyingiz   '  +  kino)
+alert ('taom   '  + ee)
+alert ('Yoqtirgan faslingiz   '   +   fasl)
+alert ('Tanlaganingiz    '  +   tanlash)
+alert ('Sizning jamoyingiz   '  +  team)
+alert ('dostingiz   '  + bro)
+alert ('Sizni hobbiyingiz   '   +  hobbi)
+alert ('Tanlagan tilingiz'   + language)
+
+
+// console.log(wer);  
+
+// console.log(wer, '+', wert1, 'wer');
+
